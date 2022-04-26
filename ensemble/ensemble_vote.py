fdaa2b7b7e7207ef5d9d1f75ac16d32a9429332e
AsteriaChiang <AsteriaChiang@outlook.com>
2022-04-26 08:42:03 +0800

    add evaluation results

--- ensemble/ensemble_vote.py
+++ ensemble/ensemble_vote.py
@@ -1,19 +1,19 @@
 import json
 
-with open("spans-pred-bert.txt") as bert,open("spans-pred-electra.txt") as electra,open("spans-pred-roberta.txt") as roberta, open("ensemble_vote.txt","w") as output:
+with open("results/distilbert-spans-pred.txt") as distilbert,open("results/spanbert_token_spans-pred.txt") as spanbert,open("results/roberta_token_spans-pred.txt") as roberta, open("ensemble_vote.txt","w") as output:
     i = 0
     while True:
-        bert_in = bert.readline()
-        electra_in = electra.readline()
+        distilbert_in = distilbert.readline()
+        spanbert_in = spanbert.readline()
         roberta_in = roberta.readline()
-        if not bert_in:
+        if not distilbert_in:
             break
-        bert_in = set(json.loads(bert_in.split("\t")[1]))
-        electra_in = set(json.loads(electra_in.split("\t")[1]))
+        distilbert_in = set(json.loads(distilbert_in.split("\t")[1]))
+        spanbert_in = set(json.loads(spanbert_in.split("\t")[1]))
         roberta_in = set(json.loads(roberta_in.split("\t")[1]))
-        result1 = bert_in.intersection(electra_in)
-        result2 = bert_in.intersection(roberta_in)
-        result3 = electra_in.intersection(roberta_in)
+        result1 = distilbert_in.intersection(spanbert_in)
+        result2 = distilbert_in.intersection(roberta_in)
+        result3 = spanbert_in.intersection(roberta_in)
         result = result1.union(result2).union(result3)
         result = list(result)
         result.sort()
--- ensemble/evaluation.py
+++ ensemble/evaluation.py
@@ -3,6 +3,31 @@ import numpy as np
 import gc
 import json
 
+# F1 score
+def system_precision_recall_f1(toxic_char_preds, gold_char_offsets):
+    def per_post_precision_recall_f1(predictions, gold):
+        if len(gold) == 0:
+            return [1.0, 1.0, 1.0] if len(predictions) == 0 else [0.0, 0.0, 0.0]
+
+        if len(predictions) == 0:
+            return [0.0, 0.0, 0.0]
+
+        predictions_set = set(predictions)
+        gold_set = set(gold)
+        nom = len(predictions_set.intersection(gold_set))
+        precision = nom / len(predictions_set)
+        recall = nom / len(gold_set)
+        f1_score = (2 * nom) / (len(predictions_set) + len(gold_set))
+
+        return [float(precision), float(recall), float(f1_score)]
+
+    # get the respective metrics per post
+    precision_recall_f1_scores = [per_post_precision_recall_f1(toxic_offsets, gold_offsets) for
+                                  toxic_offsets, gold_offsets in zip(toxic_char_preds, gold_char_offsets)]
+
+    # compute average precision, recall and f1 score of all posts
+    return np.array(precision_recall_f1_scores).mean(axis=0)
+
 def f1(preds,trues):
     if len(trues) == 0:
         return 1. if len(preds) == 0 else 0.
@@ -22,26 +47,14 @@ def avg_f1(preds,trues):
 
 test_set = pd.read_csv("../data/tsd_test.csv")
 test_set['spans'] = test_set['spans'].apply(lambda x : json.loads(x))
-train_set = pd.read_csv("../data/tsd_train.csv")
-train_set['spans'] = train_set['spans'].apply(lambda x : json.loads(x))
+# train_set = pd.read_csv("../data/tsd_train.csv")
+# train_set['spans'] = train_set['spans'].apply(lambda x : json.loads(x))
 
-# toxic_span_dataset = test_set.append(train_set,ignore_index=True)
-# toxic_span_dataset['text'] = toxic_span_dataset['text'].apply(lambda x : x.lower())
-#
-# texts = toxic_span_dataset['text'].to_numpy()
-# all_spans = toxic_span_dataset['spans'].to_numpy()
-# kf = KFold(n_splits=5)
-# train_test_indices = []
-# for train_index,test_index in kf.split(texts):
-#     train_test_indices.append((train_index,test_index))
-#
-# train_index,test_index = train_test_indices.pop()
-# spans_test = all_spans[test_index]
-#
-# print(spans_test)
-
-result =  pd.read_csv("../spans-pred.txt",sep="\t", header=None)
+result =  pd.read_csv("ensemble_vote.txt",sep="\t", header=None)
 result[1] = result[1].apply(lambda x : json.loads(x))
 
-f1_toxic = avg_f1(test_set['spans'].to_numpy(),result[1].to_numpy())
-print("test F1 = %f"%(f1_toxic))
+# f1_toxic = avg_f1(test_set['spans'].to_numpy(),result[1].to_numpy())
+# print("test F1 = %f"%(f1_toxic))
+
+test_scores=system_precision_recall_f1(test_set['spans'].to_numpy(), result[1].to_numpy())
+print(f'\nPrecision: {test_scores[0]}, Recall: {test_scores[1]}, F1: {test_scores[2]}')
